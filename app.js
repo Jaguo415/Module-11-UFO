80c741a3fd2cc52cfee4bf309d720b7b6f9792ec
Jaguo415 <83923903+Jaguo415@users.noreply.github.com>
2021-09-03 19:45:41 -0700

    Challenge Complete

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ function buildTable(data) {
   function handleClick() {
     // Grab the datetime value from the filter
     let date = d3.select("#datetime").property("value");
+    let city = d3.select("#city").property("value");
+    let state = d3.select("#state").property("value");
+    let country = d3.select("#country").property("value");
+    let shape = d3.select("#shape").property("value");
     let filteredData = tableData;
   
      // Check to see if a date was entered and filter the
@@ -34,7 +38,22 @@ function buildTable(data) {
       // Apply `filter` to the table data to only keep the
       // rows where the `datetime` value matches the filter value
       filteredData = filteredData.filter(row => row.datetime === date);
-    };
+    }
+    if (city){
+      filteredData = filteredData.filter(row => row.city === city);
+    }
+
+    if (state){
+      filteredData = filteredData.filter(row => row.state === state);
+    }
+
+    if (country){
+      filteredData = filteredData.filter(row => row.country === country);
+    }
+
+    if (shape){
+      filteredData = filteredData.filter(row => row.shape === shape);
+    }
   
      // Rebuild the table using the filtered data
     // @NOTE: If no date was entered, then filteredData will
